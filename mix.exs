1a1a41d01fad4c7f49d82a20351d4827bac8e127
maximkuzmin <photographer.max@gmail.com>
2019-10-07 03:32:30 +0700

    prod release added to mix.exs

--- mix.exs
+++ mix.exs
@@ -10,7 +10,13 @@ defmodule MySite.MixProject do
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
-      deps: deps()
+      deps: deps(),
+      releases: [
+        prod: [
+          include_executables_for: [:unix],
+          applications: [runtime_tools: :permanent]
+        ]
+      ]
     ]
   end
 
