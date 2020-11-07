156054dd33f88e2866a2d7fa69b41da44ba64537
maximkuzmin <photographer.max@gmail.com>
2020-11-08 00:35:53 +0300

    Rough layout

--- lib/my_site_web/views/layout_view.ex
+++ lib/my_site_web/views/layout_view.ex
@@ -1,3 +1,13 @@
 defmodule MySiteWeb.LayoutView do
   use MySiteWeb, :view
+
+  def show_flash_by_val(css_class, flash_key, conn) do
+    flash_val = get_flash(conn, flash_key)
+
+    if flash_val do
+      content_tag(:p, flash_val, class: css_class)
+    else
+      ""
+    end
+  end
 end
